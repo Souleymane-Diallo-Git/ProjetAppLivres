4b5d49bad69ef3d864b681694a92e111d508e68b
Souleymane Diallo <souleymane.diallo2@monboreal.ca>
2024-10-19 22:19:44 -0400

    Modification et ajout de la classe Roman

--- AppConsoleLivres/Program.cs
+++ AppConsoleLivres/Program.cs
@@ -9,9 +9,17 @@ internal class Program
         Revue revue2 = new Revue("Origine des espèces", "Charles Darwin",300, 2, 1859);
         Revue revue3 = new Revue("Pénicilline", "Alexandre Fleming", 250,3, 1955);
 
+        Roman roman1 = new Roman("L'Enfant noir", "Camara Laye", 192, "Roman autobiographique");
+        Roman roman2 = new Roman("Le Cercle des tropiques", "Alioum Fantouré", 208, "Roman politique");
+        Roman roman3 = new Roman("Le Terroriste noir", "Tierno Monénembo", 224, "Biographie romancée");
+
         // Affichez les détails de chaque revue
         revue1.AfficherDetails();
         revue2.AfficherDetails();
         revue3.AfficherDetails();
+
+        roman1.AfficherDetails();
+        roman2.AfficherDetails();
+        roman3.AfficherDetails();
     }
 }
---
+++ AppConsoleLivres/Roman.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Roman : Livre
+    {
+        public string Genre { get; set; }
+
+        public Roman (string titre, string auteur, int nbrPages, string genre) : base(titre, auteur, nbrPages) 
+        {
+            Genre = genre;
+        }
+
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Le Roman : le titre est {Titre}, l'auteur est {Auteur}, le nombre de pages est {NombrePages} et le genre est {Genre}");
+        }
+    }
+}
