1f030eda0b5dde6710343ae1a015aa24ba8217e4
Souleymane Diallo <souleymane.diallo2@monboreal.ca>
2024-10-19 21:51:00 -0400

    Ajout des instances dans la classe Program.cs

--- AppConsoleLivres/Program.cs
+++ AppConsoleLivres/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using AppConsoleLivres;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Créez trois instances de la classe Revue
+        Revue revue1 = new Revue("Vaccination et pasteurisation", "Louis pasteur",200, 1, 1885);
+        Revue revue2 = new Revue("Origine des espèces", "Charles Darwin",300, 2, 1859);
+        Revue revue3 = new Revue("Pénicilline", "Alexandre Fleming", 250,3, 1955);
+
+        // Affichez les détails de chaque revue
+        revue1.AfficherDetails();
+        revue2.AfficherDetails();
+        revue3.AfficherDetails();
     }
 }
---
+++ AppConsoleLivres/Revue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+
+        public Revue(string titre, string auteur, int nbrPages, int numero, int annee) : base(titre, auteur, nbrPages) 
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+
+
+        // Le mot clé "override" indique que l'on remplace la définition de la méthode.
+        // Héritée de la classe parente par une nouvelle version dans  la classe dérivée.
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"La Revue : le titre est {Titre}, L'auteur est {Auteur}, Le nombre de pages est {NombrePages}, Le numero est {Numero} et l'année est {Annee}");
+        }
+    }
+}
